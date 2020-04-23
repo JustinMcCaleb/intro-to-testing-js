ee8462f31d0523fe02417bd10541fe0c5763443f
Justin McCaleb <justinleemccaleb@gmail.com>
2020-04-23 16:50:22 -0500

    completed exercise 8

--- code.js
+++ code.js
@@ -1,9 +1,12 @@
 // helloWorld function 
-function helloWorld() {
+   var helloWorld = function() {
     return "Hello, World!";
-}
+};
 
 function sayHello(input) {
+    if (typeof input === 'string' && isNaN(parseFloat(input))) {
     return 'Hello, ' + input + '!';
+    } else {
+        return 'Hello, World!';
+    }
 }
-
